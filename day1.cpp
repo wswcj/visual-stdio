554e202f8636170a8dd048a077ae6db05e144745
Layne <Layne@atguigu.com>
2022-09-30 19:47:03 +0800

    f c

---
+++ day1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+#include<cmath>
+#include<string>
+int main()
+{
+	int a = 0;
+	cout << "请输入一个整数:" << endl;
+	cin >> a;
+	cout << "你输入的整数为:" << a << endl;
+	return 0;
+}
