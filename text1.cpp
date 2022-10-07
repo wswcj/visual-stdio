e9b0ba396396e1d0f18d460d4ebc3dcbfe6c2f13
Layne <Layne@atguigu.com>
2022-10-07 21:43:52 +0800

    f c

---
+++ text1.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+
+    #define MAX 100
+	const int n = 10;
+	int m = 3;
+	scanf("%d", &m);
+	printf("%d\n", m);
+	printf("%d\n", MAX);
+	return 0;
+}
